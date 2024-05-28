2700087e75923af442a419edab23a5facbc2389f
icad08 <irsyadrm08@gmail.com>
2024-05-28 15:13:50 +0700

    manggil joko dan lia

--- file2.cpp
+++ file2.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 class seseorang{
     public:
-    virtual void pesan() = 0
+    virtual void pesan() = 0;
     //virtual void pesan(){
     //cout << "pesan dari seseorang" << endl;
     //}
@@ -23,5 +23,14 @@ class lia : public seseorang{
     }
 };
 int main(){
+    seseorang* obyek;
+    joko a;
+    lia b;
 
+    obyek = &a;
+    obyek->pesan();
+    obyek = &b;
+    obyek ->pesan();
+    //a.seseorang::pesan();
+    return 0;
 }
