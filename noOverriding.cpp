48701d621c1bc90ff879b4aae95891d88fd6048c
icad08 <irsyadrm08@gmail.com>
2024-05-28 15:38:07 +0700

    add class baseClass final

--- noOverriding.cpp
+++ noOverriding.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
 
+class baseClass {
+    public:
+    virtual void perkenalan() final{
+        cout << "Hello saya funtion dari base class" << endl;
+    }
+};
+
 int main(){
-    
+
 }
