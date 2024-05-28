3ef2670fed6b018ffafae0d2c63246ea3fd433b1
icad08 <irsyadrm08@gmail.com>
2024-05-28 15:58:23 +0700

    make class abstraksi dan void setXY

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 using namespace std;
 
+class abstraksiKlas{
+    private: string x, y;
+
+    public:
+
+    //method untuk mengisi nilai
+    //private member
+    void setXY(string a, string b){
+        x=a;
+        y=b;
+    }
+    //menampilan nilai
+    void display(){
+        cout << "x = " << x << endl;
+        cout << "y = " << y << endl;
+    }
+};
+
 int main(){
-    
+
 }
