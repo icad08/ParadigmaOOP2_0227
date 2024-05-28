5913ce3595f5532bdc3e7651387bb0cbe4e8f68d
icad08 <irsyadrm08@gmail.com>
2024-05-28 15:28:07 +0700

    add BaseClass

--- noPewarisan.cpp
+++ noPewarisan.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
 
+class baseClass final{
+    public:
+    virtual void perkenalan(){
+        cout << "Hello saya funtion dari base class" << endl;
+    }
+};
+
 int main(){
-    
+
 }
